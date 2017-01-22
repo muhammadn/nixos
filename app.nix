9565a95088d8658c8bb217eb44afb1cbdf79936b
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2017-01-22 23:43:13 +0800

    some fixes

--- app.nix
+++ app.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = pkgs.fetchgit {
     url = "git@github.com:muhammadn/zalora.git";
-    sha256 = "0cff0xfni3nbkdsjkpialdjs598g3h0pkn0656i5z3bdxzzl3bpv";
+    sha256 = "0skvyi6igcf95v287qyqxwjzg5lk2rxas11rvky8k1fzw7fiybpw";
   };
 
   buildInputs = [ php php70Packages.composer ];
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
     mkdir -p /tmp/zalora/storage/app /tmp/zalora/storage/framework/cache /tmp/zalora/storage/framework/views /tmp/zalora/storage/logs /tmp/zalora/database
     touch /tmp/zalora/database/database.sqlite
     cp .env.sqlite .env
-    chmod 777 /tmp/zalora/storage /tmp/zalora/storage/logs /tmp/zalora/storage/app /tmp/zalora/storage/framework /tmp/zalora/storage/framework/cache 
+    chmod 777 /tmp/zalora/database /tmp/zalora/storage /tmp/zalora/storage/logs /tmp/zalora/storage/app /tmp/zalora/storage/framework /tmp/zalora/storage/framework/cache /tmp/zalora/database/database.sqlite
     composer install
     php artisan migrate
     mkdir -p $out
